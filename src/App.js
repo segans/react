ffae8b67e33fd04cad2411c2d95ead74bc963103
segans <111822342+segans@users.noreply.github.com>
2023-03-09 19:34:37 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import Header from './components/header';
+import Footer from './components/footer';
+
+function App() {
+  return (
+  <>
+    <Header />
+    <div>
+      CONTEUDO DAS PAGINAS
+    </div>
+    <Footer />
+  </>
+  );
+}
+export default App;
